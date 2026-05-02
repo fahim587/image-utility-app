51f4e8a9f481cc9c24b5e7561d48de6f992cec9b
Fahim587 <fahimuddinn9@gmail.com>
2026-05-02 23:14:32 +0600

    vercel problem

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,25 @@ import tailwindVite from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindVite()],
+
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
     include: ["jspdf", "pdfjs-dist/legacy/build/pdf"]
   },
+
   server: {
     hmr: { overlay: false },
     headers: {
-      // ✅ পপ-আপ এবং FFmpeg উভয়ের জন্য এটি সেরা কম্বিনেশন
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
-      "Cross-Origin-Embedder-Policy": "credentialless" 
+      "Cross-Origin-Embedder-Policy": "credentialless"
     }
   },
+
   build: {
-    chunkSizeWarningLimit: 1500
+    chunkSizeWarningLimit: 1500,
+
+    rollupOptions: {
+      external: ["onnxruntime-web/webgpu"]
+    }
   }
 });
